1ebe58b613a4cc3caa4f34977b060312e51b0cab
evelyndjwilliams <evelyndjw@gmail.com>
2022-09-28 10:58:23 +0100

    started refactoring

--- lstm.py
+++ lstm.py
@@ -1,18 +1,5 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-import librosa
-import librosa.display
-import matplotlib.pyplot as plt
-import os
-import torchaudio
-import numpy as np
-import argparse
-import shutil
-import pandas as pd
-from torchsummary import summary
 
 class LSTM(nn.Module):
     """ Defines architecture and training procedure for model.
@@ -21,45 +8,21 @@ class LSTM(nn.Module):
     def __init__(self):
         """Model architecture"""
         super().__init__()
-        # self.forward = self.model.forward()
         self.lstm = nn.LSTM(input_size=80, hidden_size=182, num_layers=1, batch_first=True)
-        # make sure batch is first dimension in input tensor, or change this
         self.linear1 = nn.Linear(182,182)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.25)
         self.linear2 = nn.Linear(182,3)
-        # self.softmax = nn.Softmax(dim=1) # not using softmax because CrossEntropyLoss applies log softmax
 
 
     def forward(self, input_data):
         x = self.lstm(input_data)
-        # print(f"after lstm part 0: {x[0].size()}")
-        # print(f"after lstm part 1 0 : {x[1][0].size()}")
-        # print(f"after lstm part 1 1 : {x[1][1].size()}")
-        # this is using final hidden state (not cell state) as input to linear
-        # as here https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html?highlight=lstm#torch.nn.LSTM
         x = self.linear1(x[1][0])
-        # print(f"after linear 1: {x[0].size()}")
         x = self.relu(x)
-        # print(f"after relu: {x[0].size()}")
         x = self.dropout(x)
-        # print(f"after dropout: {x[0].size()}")
         x = self.linear2(x)
-        # print(f"after linear 2: {x[0].size()}")
-        predictions = x
-        return predictions.squeeze(0)
+        logits = x
+        return logits.squeeze(0)
 
-
-
-
-    # data = AudioDataset(INFO_FILE, DATA_DIR)
-    # # print(f"there are {data.get_dataset_len()} samples in the dataset")
-
-    # signal, label = data[0]
-    # # print(signal[0].size())
-
-    # lstm = LSTM()
-    # predictions = lstm(signal.transpose(0,1))
-    # # print(predictions)
 
 
--- train.py
+++ train.py
@@ -17,40 +17,11 @@ import shutil
 import pandas as pd
 from lstm import LSTM
 from cnn import CNN
-# from lstm import AudioDataset
 from sklearn.preprocessing import OneHotEncoder
-from livelossplot import PlotLosses
 import numpy as np
 import gc
 
-# def train_single_epoch(model, data_loader, loss_fn, optimiser, device, mode):
-#     onehot_encoder = OneHotEncoder(sparse=False)
-#     running_loss = 0.0
-#     batch = 0
-#     for input, target in data_loader:
-#         optimiser.zero_grad()
-#         one_hot = np.zeros((target.size()[0], 3))
-#         rows = np.arange(len(target))
-#         one_hot[rows, target] = 1
-#         input, target = input.to(device), torch.Tensor(one_hot).to(device)#.unsqueeze(0).transpose(0,1)
-#         prediction = model(input).transpose(0,1)
-#         print(f'prediction size: {prediction.size()}, target size: {target.size()}')
-#         loss = loss_fn(prediction.squeeze(1), target)
-#         if mode == 'eval':
-#             print(loss)
-#             scheduler.step()
-#         running_loss += loss.item()
-#         batch +=1
-#         if mode == 'train':
-#             loss.backward()
-#             optimiser.step()  
-#             gc.collect()
-
-#     print(f"loss: {loss.item()}")
-#     print(f'avg {mode} loss = {running_loss/batch}')
-#     return running_loss/batch
-
-def train_single_epoch_cnn(model, data_loader, loss_fn, optimiser, device, mode):
+def train_single_epoch(model, data_loader, loss_fn, optimiser, device, mode):
     onehot_encoder = OneHotEncoder(sparse=False)
     running_loss = 0.0
     batch = 0
@@ -62,93 +33,50 @@ def train_single_epoch_cnn(model, data_loader, loss_fn, optimiser, device, mode)
         target = one_hot
         target = torch.Tensor(target).to(device).squeeze(0)
         prediction = model(input).to(device)
-        print(f'prediction size: {prediction.size()}, target size: {target.size()}')
+        # print(f'prediction size: {prediction.size()}, target size: {target.size()}')
         loss = loss_fn(prediction, target)
-        print(loss.item())
-        if mode == 'eval':
-            scheduler.step()
+        # print(loss.item())
         running_loss += loss.item()
-        
         if mode == 'train':
             loss.backward()
             optimiser.step() 
             gc.collect()
         batch +=1
-    print(f"loss: {loss.item()}")
+    # print(f"loss: {loss.item()}")
     print(f'avg {mode} loss = {running_loss/batch}')
     return running_loss/batch
 
-# def train(model, train_loader, val_loader, loss_fn, optimiser, device, epochs, log_file):
-#     train_losses = []
-#     val_losses = []
-#     with open(log_file, 'w+') as o:
-#         pass
-#     for i in range(epochs):
-#         # logs = {}
-#         model.train()
-#         print(f"Epoch {i+1}")
-#         train_loss = train_single_epoch(model, train_loader, loss_fn, optimiser, device, 'train')
-#         train_losses.append(train_loss)
-#         model.eval()
-#         val_loss = train_single_epoch(model, val_loader, loss_fn, optimiser, device, 'eval')
-#         val_losses.append(val_loss)
-#         print("---------------------------")
-#         plt.plot(train_losses, linestyle = 'dotted', label='train')
-        
-
-#         # plt.plot(x1, y1, label = "line 1")
-#         # line 2 points
-#         # x2 = [10,20,30]
-#         # y2 = [40,10,30]
-#         # plotting the line 2 points 
-#         plt.plot(val_losses, linestyle = 'dotted', label = "val")
-#         plt.xlabel('Epoch')
-#         # Set the y axis label of the current axis.
-#         plt.ylabel('Cross Entropy Loss')
-#         # Set a title of the current axes.
-#         plt.title('Cross Entropy Loss computed on audio training and validation sets.')
-#         # show a legend on the plot
-#         if i == 0:
-#             plt.legend()
-#         plt.savefig(f'lstm training curve.png')
-#         with open(log_file, 'a') as o:
-#             o.write(f'Epoch {i+1}\n Train loss: {train_loss} \n Val loss: {val_loss}')
-#     print("Finished training")
-
-def train_cnn(model, train_loader, val_loader, loss_fn, optimiser, device, epochs, log_file):
+def train(model, train_loader, val_loader, loss_fn, optimiser, device, epochs, log_file):
     best_val_loss = 1000
     train_losses = []
     val_losses = []
     with open(log_file, 'w+') as o:
         pass
     for i in range(epochs):
-        # logs = {}
-        model.train()
         print(f"Epoch {i+1}")
-        train_loss = train_single_epoch_cnn(model, train_loader, loss_fn, optimiser, device, 'train')
+        model.train()
+        train_loss = train_single_epoch(model, train_loader, loss_fn, optimiser, device, 'train')
         train_losses.append(train_loss)
         model.eval()
-        val_loss = train_single_epoch_cnn(model, val_loader, loss_fn, optimiser, device, 'eval')
-        if val_loss <= best_val_loss:
-            torch.save(model.state_dict(), f"best_val_loss_cnn_{i+1}.pth")
+        val_loss = train_single_epoch(model, val_loader, loss_fn, optimiser, device, 'eval')
+        """ store model with lowest loss on val set"""
+        if val_loss < best_val_loss:
+            torch.save(model.state_dict(), f"{log_file[:4]}_best_val_loss_epoch_{i+1}.pth")
+            best_val_loss = val_loss
         val_losses.append(val_loss)
-        print("---------------------------")
+        """Update and save training curves"""
         plt.plot(train_losses, linestyle = 'dotted', label='train', color='green')
         plt.plot(val_losses, linestyle = 'dotted', label = "val", color='orange')
         plt.xlabel('Epoch')
-        # Set the y axis label of the current axis.
         plt.ylabel('Cross Entropy Loss')
-        # Set a title of the current axes.
         plt.title('Cross Entropy Loss computed on audio training and validation sets.')
-        
-        # show a legend on the plot
         if i == 0:
-        # legend.remove()
             plt.legend()
-        plt.savefig(f'cnn training curve.png')
+        plt.savefig(f'cnn training curve no dropout.png')
         with open(log_file, 'a') as o:
             o.write(f'Epoch {i+1}\n Train loss: {train_loss} \n Val loss: {val_loss}\n')
-        torch.save(model.state_dict(), f"{log_file[:5]}.pth")
+        scheduler.step()
+        print("---------------------------")
     print("Finished training")
 
 class Preprocessor():
@@ -157,23 +85,16 @@ class Preprocessor():
         To run preprocessing, pass --preprocess arg to train.py"""
 
     def __init__(self):
-        # self.__init__()
-        self.transform = torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_fft=2048, hop_length=265, n_mels=80)
+        self.transform = torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_fft=2048, hop_length=512, n_mels=80)
         self.labels = {'speech':0, 'rap':1, 'singing':2}
 
     def preprocess(self, filename):
-        # wav, sr = librosa.load(fn, sr=16000)
-        # wav = librosa.effects.preemphasis(wav) # pre-emphasis is messing up singing spectrograms - huge blown-out high frequencies
         wav, sr = torchaudio.load(filename, normalize=True)
-        # transform = torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_fft=2048, hop_length=265, n_mels=182)
         ms = self.transform(wav)
         log_ms = librosa.amplitude_to_db(ms.squeeze(0))
         mm_scaler = MinMaxScaler()
-        mm_scaler.fit(log_ms)#.squeeze(0))
-        scaled_log_ms = mm_scaler.transform(log_ms)#.squeeze(0))
-        # plt.imshow(scaled_log_ms, aspect='auto')
-        # plt.gca().invert_yaxis()
-        # plt.show()
+        mm_scaler.fit(log_ms)
+        scaled_log_ms = mm_scaler.transform(log_ms)
         return torch.Tensor(scaled_log_ms)
 
     def preprocess_dir(self, in_dir, out_dir, info_file):
@@ -185,7 +106,6 @@ class Preprocessor():
                 if genre.startswith('.'):
                     continue
                 genre_dir = os.path.join(in_dir, genre)
-                # if not os.path.exists(os.path.join(out_dir, genre)):
                 os.mkdir(os.path.join(out_dir, genre))
                 for person in os.listdir(genre_dir):
                     person_dir = os.path.join(genre_dir, person)
@@ -222,102 +142,95 @@ if __name__ == "__main__":
     parser.add_argument('--lstm', action='store_true')
     args = parser.parse_args()
 
+    """Hyperparameters"""
+
+    SAMPLE_RATE = 16000
+    BATCH_SIZE = 24
+    EPOCHS = 100
+    LR = 0.1
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    LOSS_FN = nn.CrossEntropyLoss()
+
+    """Preprocess data (mel-spectrogram generation)"""
+
     DATA_DIR = 'preprocessed_data'
     INFO_FILE = os.path.join(DATA_DIR, 'data_info.csv')
-    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     if args.preprocess == True:
         data_preprocessor = Preprocessor()
         data_preprocessor.preprocess_dir("data", DATA_DIR, INFO_FILE)
 
-    # hyperparameters
-    SAMPLE_RATE = 16000
-    BATCH_SIZE = 24
-    EPOCHS = 100
-    LR = 0.0001
-    
-    DATA_DIR = 'preprocessed_data'
-    INFO_FILE = os.path.join(DATA_DIR, 'data_info.csv')
-    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    LOSS_FN = nn.CrossEntropyLoss()
-    
+    """Create, shuffle & split training sets"""
 
     data = AudioDataset(INFO_FILE, DATA_DIR)    
+
     val_amount = 0.1
     test_amount = 0.1
     dataset_size = len(data)
     indices = list(range(dataset_size))
     val_split = int(np.floor(val_amount * dataset_size))
     test_split = val_split + int(np.floor(test_amount * dataset_size))
-    shuffle_dataset = True
+
     random_seed = 42
-    if shuffle_dataset :
-        np.random.seed(random_seed)
-        np.random.shuffle(indices)
+    np.random.seed(random_seed)
+    np.random.shuffle(indices)
     train_indices, val_indices, test_indices = indices[test_split:], indices[:val_split], indices[val_split:test_split]
+
+    # storing indices of test set
     with open ('test_indices.csv', 'w+') as o:
         for i in test_indices:
             o.write(f'{i} \n')
-    """Creating Torch data samplers and loaders:"""
+
     train_sampler = SubsetRandomSampler(train_indices)
     val_sampler = SubsetRandomSampler(val_indices)
+
     train_loader = DataLoader(data,
                                     batch_size=BATCH_SIZE,
                                     sampler=train_sampler,
-                                    drop_last=True)
-    val_batch_size = 1 if args.cnn == True else len(val_indices)   
+                                    drop_last=True,
+                                    num_workers=4)
+    
     val_loader = DataLoader(data,
                                     batch_size=BATCH_SIZE,
                                     sampler=val_sampler,
-                                    drop_last=True)
+                                    drop_last=True,
+                                    num_workers=4)
     val_set = Subset(data, val_indices)
     loader = DataLoader(data, batch_size=BATCH_SIZE)
 
+    """Train models"""
+
     if args.lstm== True:
-        lstm = LSTM()
-        # optimiser = torch.optim.Adam(lstm.parameters(),lr=LR)
-        # optimiser = optim.SGD(lstm.parameters(), lr=LR, momentum=0.9)
-        optimiser = optim.SGD(lstm.parameters(), 0.1)
-        scheduler = optim.lr_scheduler.ExponentialLR(optimiser, gamma=0.9)
-        # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimiser)
-        train_cnn(lstm, train_loader, val_loader, LOSS_FN, optimiser, DEVICE, EPOCHS, 'lstm_log.txt')
-        torch.save(lstm.state_dict(), "lstm.pth")
-        print("Trained lstm saved at lstm.pth")
+        model = LSTM()
+        model_name = 'lstm'
 
     if args.cnn== True:
-        cnn = CNN()
-        # optimiser = torch.optim.Adam(cnn.parameters(),lr=LR)
-        # optimiser = optim.SGD(cnn.parameters(), lr=LR, momentum=0.9)
-        # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimiser)
-        optimiser = optim.SGD(cnn.parameters(), 0.1)
-        scheduler = optim.lr_scheduler.ExponentialLR(optimiser, gamma=0.8)
-        train_cnn(cnn, train_loader, val_loader, LOSS_FN, optimiser, DEVICE, EPOCHS, 'cnn_log.txt')
-        torch.save(cnn.state_dict(), "cnn.pth")
-        print("Trained cnn saved at cnn.pth")
+        model = CNN()
+        model_name = 'cnn'
+
+    optimiser = optim.SGD(model.parameters(), LR)
+    scheduler = optim.lr_scheduler.ExponentialLR(optimiser, gamma=0.95)
+    train(model, train_loader, val_loader, LOSS_FN, optimiser, DEVICE, EPOCHS, f'{model_name}_log.txt')
+    torch.save(model.state_dict(), f"{model_name}.pth")
+    print(f"Trained {model_name} saved at {model_name}.pth")
 
 
 # TO DO
 
-# TRAINING SETS
-# randomise shuffle and split data : 80% train, 10% val, 10% test
-# shuffle labels and data same way
-
-# PLOTTING
-# plot valation loss curve
+# URGENT 
+# backup to gh
 
 # TRAINING METHOD
-# set scheduler / annealing --> test
-# set scheduler to step() using val loss
-# set adaptive LR
-#
+# hypterparameter tuning: reduce LR : (0.1, 0.05, 0.01)
+# change gamma lav for scheduler
+# if val loss is bouncing around : reduce LR
 # TRY CNN
-# if CEL still bad --> look at loss : softmax, or chnge loss function
+# compare dropout / no dropout (backup loss curves to compare)
 
 # CODE REFACTORING
 # move hyperparams to dict file
 # comment for readability
-# exclude all LSTM stuff
-# TRY MFCCs + deltas
+# exclude all LSTM stuff - just check it runs
 
 
 
